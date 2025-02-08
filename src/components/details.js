0866f608b275773ee8e1edbb0e184d189fa5f74d
Splashley <hynes_ashley@hotmail.com>
2025-02-08 13:38:02 -0500

    created components

---
+++ src/components/details.js
@@ -0,0 +1,13 @@
+export default function Details() {
+
+    return (
+        <>
+            <div className="grid grid-cols-2 overflow-y-auto py-8 bg-white">
+                <div className="bg-pink-500"></div>
+                <div className="text-black p-12">
+                    <h2>hi,</h2>
+                    <p>my name is splashley and blah blah blah.</p></div>
+            </div>
+        </>
+    );
+}
